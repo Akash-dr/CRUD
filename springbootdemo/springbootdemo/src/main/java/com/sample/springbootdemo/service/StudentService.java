56662d64d7f1659a8eef2f1e4bf705e1e77b2732
Akash-dr <akashdr5603@gmail.com>
2026-01-15 13:05:40 +0530

    get,post

--- springbootdemo/springbootdemo/src/main/java/com/sample/springbootdemo/service/StudentService.java
+++ springbootdemo/springbootdemo/src/main/java/com/sample/springbootdemo/service/StudentService.java
@@ -1,7 +1,12 @@
 package com.sample.springbootdemo.service;
 
 
+import com.sample.springbootdemo.model.StudentModel;
 
+import java.util.List;
 
 public interface StudentService {
+    public StudentModel insertNewStudent(StudentModel model);
+    public List<StudentModel> viewAllStudents();
+    public StudentModel viewOneStudent(Integer studentId);
 }
